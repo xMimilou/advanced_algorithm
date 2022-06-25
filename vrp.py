3848bd9d78da34be40ea1c834082f77c11919a4d
xMimilou <Louka06.millon@gmail.com>
2022-06-25 11:07:59 +0200

    calcule de distance entre les points

--- vrp.py
+++ vrp.py
@@ -1,7 +1,9 @@
 import random
-
+import math
 grid_size = 10
-nb_ville = 10
+nb_ville = 2
+nb_camion = 4
+
 
 def random_city():
     coordinate_ville = [(random.randint(1,grid_size),random.randint(1,grid_size)) for i in range(nb_ville)]
@@ -9,3 +11,16 @@ def random_city():
     coordinate_ville.insert(0,base_coordinate)
     return coordinate_ville
 
+def distance_between_coord(coord_ville):
+    table_distance_point = []
+    for coord in coord_ville:        
+        list_distance_point = []
+        for point in coord_ville:
+            coord_x = (point[0]-coord[0])
+            coord_y = (point[1]-coord[1])
+            dist = math.sqrt(coord_x**2+coord_y**2)
+            list_distance_point.append(dist)
+        table_distance_point.append(list_distance_point)
+    return table_distance_point
+
+print(distance_between_coord(random_city()))
