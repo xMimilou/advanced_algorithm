b4bd8e26142f64740a1f8f9e202e9d1a39d5ba37
xMimilou <Louka06.millon@gmail.com>
2022-06-25 10:01:29 +0200

    function random_city

--- vrp.py
+++ vrp.py
@@ -1,3 +1,11 @@
-for 
+import random
 
-coordinate = [(0,0),  ]
+grid_size = 10
+nb_ville = 10
+
+def random_city():
+    coordinate_ville = [(random.randint(1,grid_size),random.randint(1,grid_size)) for i in range(nb_ville)]
+    base_coordinate = (0,0)
+    coordinate_ville.insert(0,base_coordinate)
+    return coordinate_ville
+
